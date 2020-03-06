2bf0c84cd57786b8cf375530a523bbda0702f819
Alex <du.alexcruz@gmail.com>
2020-03-06 12:48:20 -0500

    extras: Navbar: Introduce navbar styles [3/3]
    Change-Id: I438dabcbc1b441cdbe0746d315b8536ab97ce0d5

--- shishu.mk
+++ shishu.mk
@@ -109,6 +109,14 @@ PRODUCT_PACKAGES += \
     IconPackAcherusAndroidOverlay \
     IconPackAcherusSystemUIOverlay
 
+# Navbar
+PRODUCT_PACKAGES += \
+    NavbarAsusOverlay \
+    NavbarOnePlusOverlay \
+    NavbarOneUiOverlay \
+    NavbarStockOverlay \
+    NavbarTecnoCamonOverlay
+
 # Theme-Required overlays packages
 # PRODUCT_PACKAGES += \
 #     AOSPThemeAndroidOverlay \
